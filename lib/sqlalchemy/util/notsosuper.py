f3eb49008e93ab220e5c202460e4773c86b8a290
NotSoSuper <xnotsosuper@gmail.com>
2016-11-18 15:19:35 -0500

    final

--- lib/sqlalchemy/util/notsosuper.py
+++ lib/sqlalchemy/util/notsosuper.py
@@ -1,11 +1,13 @@
 #All functions modified from https://github.com/PyMySQL/PyMySQL/blob/master/pymysql/
 #This is all modified to my specific needs, sorry if it gives you any form of sickness.
+#Note for me: you changed pymysql execute function too, removed escaping (mog)
 from pymysql.converters import encoders
 from pymysql.converters import _escape_unicode as escape_string
 from pymysql.charset import charset_by_name
 from functools import partial
 
 encoding = charset_by_name('utf8mb4').encoding
+charset = 'utf8mb4'
 
 def literal(obj):
 	return escape(obj, encoders)
@@ -29,9 +31,9 @@ def _escape_args(args):
 def escape(obj, mapping=None):
 	if isinstance(obj, str):
 		return "'" + escape_string(obj) + "'"
-	return escape_item(obj, self.charset, mapping=mapping)
+	return escape_item(obj, charset, mapping=mapping)
 
-def escape_item(val, charset, mapping=None):
+def escape_item(val, c, mapping=None):
 	if mapping is None:
 		mapping = encoders
 	encoder = mapping.get(type(val))
@@ -41,7 +43,21 @@ def escape_item(val, charset, mapping=None):
 		except KeyError:
 			raise TypeError("no default type converter defined")
 	if encoder in (escape_dict, escape_sequence):
-		val = encoder(val, charset, mapping)
+		val = encoder(val, c, mapping)
 	else:
 		val = encoder(val, mapping)
 	return val
+
+def escape_dict(val, c, mapping=None):
+	n = {}
+	for k, v in val.items():
+		quoted = escape_item(v, c, mapping)
+		n[k] = quoted
+	return n
+
+def escape_sequence(val, c, mapping=None):
+	n = []
+	for item in val:
+		quoted = escape_item(item, c, mapping)
+		n.append(quoted)
+	return "(" + ",".join(n) + ")"
